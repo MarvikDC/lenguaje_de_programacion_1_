8994373973270108e24d74198302d67dba4ca9a8
Marvik <mdelcarpiol@ulasalle.edu.pe>
2018-09-30 14:33:20 -0500

    e

--- laboratorio 2/try_this06.cpp
+++ laboratorio 2/try_this06.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include<vector>
+#include<string>
 #include<algorithm>
 
 using namespace std;
@@ -8,11 +9,11 @@ int main()
 {
     vector<string>censura;
     vector<string>frases;
-    string r,fin="a",p="piiii"
-    int tfrase=0;
+    string r,fin="a",p="piiii";
+    int tfrase=0,i,j;
     /*while(cin>>r&&r!="p")
         pi.push_back(i);*/
-    string a="carajo",b=mierda,c="jodete",d="maldito";
+    string a="carajo",b="mierda",c="jodete",d="maldito";
     censura.push_back(a);
     censura.push_back(b);
     censura.push_back(c);
@@ -22,20 +23,20 @@ int main()
         cin>>r;
         frases.push_back(r);
     }
-
-    while(tfrase<=frases.size())
+   for(j=0;j<=frases.size();++j)
     {
-       for (i=0,i<=censura.size();++i)
+       for(i=0;i<=censura.size();++i)
         {
-            if (frases[tfrase]==censura[i])
-            {frases[tfrase]="p";
+            if (frases[j]==censura[i])
+            {
+                frases[j]=p;
                 break;
             }
         }
-        tfrase+=1
+       // tfrase+=1
     }
-    for(j=0, j<=frases.size(),++j)
-        cout<<frases[j];
+    for(j=0; j<=frases.size();++j)
+        cout<<frases[j]<<" ";
 
     return 0;
 }
